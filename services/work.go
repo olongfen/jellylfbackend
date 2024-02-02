891aa40c7057ccc305a9f6e820c546692d3b8e52
olongfen <olongfen@gmail.com>
2024-02-02 10:58:53 +0800

    工作按照时间排序

--- services/work.go
+++ services/work.go
@@ -10,6 +10,6 @@ func GetWorksWithPersonID(ctx context.Context, personID uint) ([]*model.Work, er
 	var (
 		works []*model.Work
 	)
-	db := global.DB.WithContext(ctx).Model(&model.Work{}).Where("person_id = ?", personID).Find(&works)
+	db := global.DB.WithContext(ctx).Model(&model.Work{}).Where("person_id = ?", personID).Order("created_at desc").Find(&works)
 	return works, db.Error
 }
